a5d1e8a69772806b930221da9c3874d398929d59
Nick Hollas <nicholashollas@yahoo.co.uk>
2023-01-26 10:26:50 +0000

    Removal Part 2

--- Jobby.UI/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/board/[boardId]":["static\u002Fchunks\u002Fpages\u002Fboard\u002F[boardId].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fboard\u002F[boardId]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
