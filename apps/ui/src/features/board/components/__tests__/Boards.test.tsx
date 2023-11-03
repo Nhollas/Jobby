d9435c4d2337a2652d884388a58265adcf2af14a
Nick Hollas <nicholashollas@yahoo.co.uk>
2023-11-03 08:57:07 +0000

    Remove redundant code

--- apps/ui/src/features/board/components/__tests__/Boards.test.tsx
+++ apps/ui/src/features/board/components/__tests__/Boards.test.tsx
@@ -1,21 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { Boards, useBoardsQuery } from "@/features/board";
 import "@testing-library/jest-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 jest.mock("@/features/board/api");
-
-const queryClient = new QueryClient();
 
 describe("Boards", () => {
   it("displays loading state", () => {
     (useBoardsQuery as jest.Mock).mockReturnValue({ isLoading: true });
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <Boards />
-      </QueryClientProvider>
-    );
+    render(<Boards />);
 
     expect(screen.getByText("Loading...")).toBeInTheDocument();
   });
@@ -23,11 +16,7 @@ describe("Boards", () => {
   it("displays error state", () => {
     (useBoardsQuery as jest.Mock).mockReturnValue({ isError: true });
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <Boards />
-      </QueryClientProvider>
-    );
+    render(<Boards />);
 
     expect(screen.getByText("Error")).toBeInTheDocument();
   });
@@ -37,11 +26,7 @@ describe("Boards", () => {
       isSuccess: true,
     });
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <Boards />
-      </QueryClientProvider>
-    );
+    render(<Boards />);
 
     expect(screen.getByText("View and manage boards")).toBeInTheDocument();
   });
