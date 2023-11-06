968f8549aae528ef4b26b7a6de6984a87c3a8c1b
Nick Hollas <nicholashollas@yahoo.co.uk>
2023-11-06 17:46:12 +0000

    QOL temp code

---
+++ apps/ui/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/track/boards");
+  }, [router]);
+}
